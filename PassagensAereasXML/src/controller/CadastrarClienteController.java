ab37a7fbd3161e93adee138cb6ca64628ce173f4
Bryan <bryan.eduardo9@gmail.com>
2018-07-12 11:54:38 -0300

    Update 23

--- PassagensAereasXML/src/controller/CadastrarClienteController.java
+++ PassagensAereasXML/src/controller/CadastrarClienteController.java
@@ -2,20 +2,54 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.Cliente;
+import model.DAO.ClienteDAO;
 
 public class CadastrarClienteController implements Initializable {
 
     @FXML
-    private AnchorPane painelCadastrarCliente;
+    private TextField txbNome;
 
     @FXML
-    private void efetuarCadastroCliente(ActionEvent event) {
+    private TextField txbRg;
+
+    @FXML
+    private TextField txbTelefone;
+
+    @FXML
+    private Button btnCadastro;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private AnchorPane painelCadastrarCliente;
+
+    private ClienteDAO cDao = new ClienteDAO();
+    private Cliente c = new Cliente();
+
+    @FXML
+    private void efetuarCadastroCliente(ActionEvent event) throws SQLException {
+        String nome = txbNome.getText();
+        Integer rg = Integer.parseInt(txbRg.getText());
+        String contato = txbTelefone.getText();
+        
+        if(nome.equals("") || rg.equals("") || contato.equals(""))
+        
+        c.setNome(nome);
+        c.setRg(rg);
+        c.setContato(contato);
+        cDao.insert(c);
+
     }
 
     @FXML
--- PassagensAereasXML/src/controller/CadastroClienteController.java
+++ PassagensAereasXML/src/controller/CadastroClienteController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,12 +10,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.DAO.ClienteDAO;
 import model.Cliente;
 import model.bd.Conexao;
@@ -46,8 +50,12 @@ public class CadastroClienteController implements Initializable {
 
     private ObservableList<Cliente> observableListClientes;
 
+
+
     //private final Connection con = Conexao.getConnection();
     private final ClienteDAO cd = new ClienteDAO();
+
+ 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
